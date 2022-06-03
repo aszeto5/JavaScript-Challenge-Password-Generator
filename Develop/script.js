04af7786a31d7b81f06c9816eaa2ec8b789575e4
Adrian Szeto <adrianduongszeto@gmail.com>
2022-06-03 14:21:02 -0700

    added usable characters and loop for randomizer

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,23 @@
 // Assignment code here
 
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+var password = "";
+var passwordLength = 8;
+
+//generating random letters
+for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+}
+
+document.getElementById("password").value = password;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = ;
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;

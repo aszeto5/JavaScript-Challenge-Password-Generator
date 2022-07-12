6e81ab2dacd91fb123ea0e9da2b773632a025868
Adrian Szeto <adrianduongszeto@gmail.com>
2022-07-12 09:21:36 -0700

    gave each letter individual values so that it may register each character as one value instead of the whole alphabet as a single value

--- Develop/script.js
+++ Develop/script.js
@@ -2,10 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 //Global character options
-var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-var specialChars = "!@#$%^&*()'";
-var numbers = "123456789"
+var upperLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var specialChars = ['!','@','#','$','%','^','&','*','(',')','<','>','?'];
+var numbers = ['1','2','3','4','5','6','7','8','9'];
 
 //generate random password based upon criterias via series of prompts
 function generatePassword() {

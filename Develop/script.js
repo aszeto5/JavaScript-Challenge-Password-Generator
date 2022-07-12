b73fad27856f22268a731b70efad5737499f777b
Adrian Duong Szeto <adrianduongszeto@gmail.com>
2022-07-11 19:56:36 -0700

    able to pull global character types for random password generating and display result after confirming character criterias

--- Develop/script.js
+++ Develop/script.js
@@ -2,43 +2,57 @@
 var generateBtn = document.querySelector("#generate");
 
 //Global character options
-  var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-  var specialChars = "!@#$%^&*()'";
-  var numbers = "123456789"
-  
+var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+var specialChars = "!@#$%^&*()'";
+var numbers = "123456789"
+
 //generate random password based upon criterias via series of prompts
-  function generatePassword() {
-    var password = "";
+function generatePassword() {
+  var password = "";
 
 //Password Length
-    var confirmLength = prompt("How many characters do you want to include in your password?");
-    while(confirmLength <= 8 || confirmLength >= 128) {
-      alert("Password length must be between 8-128 characters.");
-      confirmLength = (prompt("How many characters do you want your password to include?"));
-    };
+  var confirmLength = prompt("How many characters do you want to include in your password?");
+  while(confirmLength <= 8 || confirmLength >= 128) {
+    alert("Password length must be between 8-128 characters.");
+    confirmLength = (prompt("How many characters do you want your password to include?"));
+  };
 
 //Variables for selected character types
-    var upperCase = confirm("Click 'OK' to include uppercased characters in your password.")
-    var lowerCase = confirm("Click 'OK' to include lowercased characters in your password.")
-    var specialCase = confirm("Click 'OK' to include special characters in your password.")
-    var numberCase = confirm("Click 'OK' to include numbers in your password.")
+  var upperCase = confirm("Click 'OK' to include uppercased characters in your password.")
+  var lowerCase = confirm("Click 'OK' to include lowercased characters in your password.")
+  var specialCase = confirm("Click 'OK' to include special characters in your password.")
+  var numberCase = confirm("Click 'OK' to include numbers in your password.")
 
-//Character Type
-    if (upperCase === false && lowerCase === false && specialCase === false && numberCase === false) {
-      window.alert("Please select at least ONE character type");
-    
-    upperCase = window.confirm("Click to inlcude upper cased letters in your password.");
-    lowerCase = window.confirm("Click to include lower cased lettters in your password.");
-    specialCase = window.confirm("Click to include special characters in your password.");
-    numberCase = window.confirm("Click to confirm numbers in your password.");
-    };
-
-  };
+//Confirm character types
+  if (upperCase === false && lowerCase === false && specialCase === false && numberCase === false) {
+    window.alert("Please select at least ONE character type");
   
+  upperCase = window.confirm("Click to inlcude upper cased letters in your password.");
+  lowerCase = window.confirm("Click to include lower cased lettters in your password.");
+  specialCase = window.confirm("Click to include special characters in your password.");
+  numberCase = window.confirm("Click to confirm numbers in your password.");
+  };
+
+
+//Running criteria
+
+  var randomLetter;
   var chars = [];
 
-  var confirmLength = window.prompt("How many characters do you want to include in your password?");
+  if (upperCase) {
+    chars = chars.concat(upperLetters);
+  }
+  if (lowerCase) {
+    chars = chars.concat(lowerLetters);
+  }
+  if (specialCase) {
+    chars = chars.concat(specialChars);
+  }
+  if (numberCase) {
+    chars = chars.concat(numbers);
+  }
+
   //randomizing letters
   for (var i = 0; i <= confirmLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
@@ -47,6 +61,8 @@ var generateBtn = document.querySelector("#generate");
 
     password = password + randomLetter;
   }
+  return password;
+};
 
 // document.getElementById("password").value = password;
 
@@ -55,10 +71,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var newpassword = generatePassword();
-  var passwordText = document.querySelector("#password");
+var newPassword = generatePassword();
+var passwordText = document.querySelector("#password");
 
-  passwordText.value = newpassword;
+passwordText.value = newPassword;
 
 };
 
